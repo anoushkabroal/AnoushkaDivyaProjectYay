c033479fd577157510e3e8a54accfd34752d9e68
anoushkabroal <anoushkabroal@gmail.com>
2023-11-01 21:25:07 -0400

    Commit First Threads
    The Commit that has 100 threads that break 100,000,000 into groups of 1,000,000

---
+++ src/AnoushkaDivyaThreads.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AnoushkaDivyaThreads{
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+
+    }
+
+}
