86404bd9a2141378a93bfd56f672fea17695fe0a
anoushkabroal <anoushkabroal@gmail.com>
2023-11-01 21:32:53 -0400

    Real First Commit All Threads
    100 threads counting 1,000,000 each and writing them to files

--- src/AnoushkaDivyaThreads.java
+++ src/AnoushkaDivyaThreads.java
@@ -2,11 +2,46 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class AnoushkaDivyaThreads{
-    public static void main(String[] args) {
-        //System.out.println("Hello world!");
+public class AnoushkaDivyaThreads implements Runnable{
+    public static void main(String args[])
+    {
+        // create an object of Runnable target
 
+        AnoushkaDivyaThreads aa = new AnoushkaDivyaThreads();
+        int count = 0;
 
+        for(int i = 1; i <= 100; i++)
+        {
+
+            // pass the runnable reference to Thread
+            Thread t = new Thread(aa, "Thread "+i);
+
+            // start the thread
+            t.start();
+
+            // get the name of the thread
+            System.out.println(t.getName());
+
+        }
+    }
+    @Override public void run()
+    {
+        //System.out.println("Inside run method for ");
+        try
+        {
+            FileWriter writer = new FileWriter(new File("C:\\Users\\gaurav\\Desktop\\Test",Thread.currentThread().getName()+".txt"));
+
+            for (int j = 1; j <= 1000000; j++)
+            {
+                writer.write(Thread.currentThread().getName()+":"+j+"\n");
+                //System.out.println(Thread.currentThread().getId()+":"+j);
+            }
+            writer.close();
+        } catch (IOException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 }
