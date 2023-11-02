93204b10fdcb6c6b8d0b870721e6430fe4a5a6e0
anoushkabroal <anoushkabroal@gmail.com>
2023-11-01 22:10:19 -0400

    Summation/Thread Counting Real
    The First Commit of threads that count to 1,000,000 and write that to a file, as well as summing them up.

--- src/AnoushkaDivyaThreads.java
+++ src/AnoushkaDivyaThreads.java
@@ -1,14 +1,30 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Timer;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+
 
 public class AnoushkaDivyaThreads implements Runnable{
+    public static double times;
+    public int count=0;
+
     public static void main(String args[])
     {
         // create an object of Runnable target
 
         AnoushkaDivyaThreads aa = new AnoushkaDivyaThreads();
         int count = 0;
+
+
+       // timeBest.start();
 
         for(int i = 1; i <= 100; i++)
         {
@@ -20,7 +36,7 @@ public class AnoushkaDivyaThreads implements Runnable{
             t.start();
 
             // get the name of the thread
-            System.out.println(t.getName());
+            //System.out.println(t.getName());
 
         }
     }
@@ -36,6 +52,9 @@ public class AnoushkaDivyaThreads implements Runnable{
                 writer.write(Thread.currentThread().getName()+":"+j+"\n");
                 //System.out.println(Thread.currentThread().getId()+":"+j);
             }
+            count += 1000000;
+            NumberFormat num = NumberFormat.getInstance();
+            System.out.println(Thread.currentThread().getName()+":"+ num.format(count));
             writer.close();
         } catch (IOException e)
         {
